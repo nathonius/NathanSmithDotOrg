f58b5e42f1c180cdb09f756f51e56edddfe223c3
Nathan Smith <Nathan.Smith@bentley.com>
2017-08-25 13:21:03 -0500

    Added ghp site

--- NathanSmithDotOrg/server.ts
+++ NathanSmithDotOrg/server.ts
@@ -1,9 +1,32 @@
 ﻿import * as Hapi from 'hapi';
+import * as Inert from 'inert';
 const server = new Hapi.Server();
 server.connection({
     host: process.env.HOST || 'localhost',
     port: process.env.PORT || 8080
 });
+
+server.register(Inert, () => {});
+
+server.route({
+    method: 'GET',
+    path: '/ghp/{path*}',
+    handler: {
+        directory: {
+            path: './public/Officerhalf.github.io',
+            listing: false,
+            index: false
+        }
+    }
+});
+
+server.route({
+    method: 'GET',
+    path: '/',
+    handler: function (request, reply) {
+        reply.file('./public/Officerhalf.github.io/index.html');
+    }
+})
 
 server.route({
     method: 'GET',
