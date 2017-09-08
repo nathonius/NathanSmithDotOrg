f455778d2a157c01ed4e0e0e7c56e697c5122d45
Nathan Smith <nathan@praisetopia.org>
2017-09-08 08:37:20 -0500

    added hover text for main icons

--- NathanSmithDotOrg/public/js/waypoints.js
+++ NathanSmithDotOrg/public/js/waypoints.js
@@ -1,10 +1,32 @@
-﻿function toggleActive(anchor) {
+﻿function showLabel(btn) {
+    if (btn)
+        var anchor = document.getElementById('icon-label-' + btn);
+    var allLabels = document.querySelectorAll('.icon-label');
+    for (var each of allLabels) {
+        each.classList.remove('is-shown');
+    }
+    if (btn)
+        anchor.classList.add('is-shown');
+}
+
+var nextAnchor = "about-page";
+
+function toggleActive(anchor) {
     var all = document.querySelectorAll('.n-nav-link');
     for (var each of all) {
         each.classList.remove('is-active');
     }
     var thisOne = document.getElementById(anchor);
     thisOne.classList.add('is-active');
+}
+
+function setArrowDir(direction) {
+    // get arrow, swap direction if need be
+}
+
+function goTo() {
+    var anchor = document.getElementById(nextAnchor);
+    scroll.animateScroll(anchor);
 }
 
 var waypointMain = new Waypoint({
@@ -14,6 +36,7 @@ var waypointMain = new Waypoint({
         for (var each of all) {
             each.classList.remove('is-active');
         }
+        nextAnchor = "about-page";
     }
 })
 
@@ -21,6 +44,7 @@ var waypointAboutTop = new Waypoint({
     element: document.getElementById('about-page'),
     handler: function () {
         toggleActive('about-link');
+        nextAnchor = "projects-page";
     },
     offset: '50%'
 });
@@ -29,6 +53,7 @@ var waypointAboutBottom = new Waypoint({
     element: document.getElementById('about-page'),
     handler: function () {
         toggleActive('about-link');
+        nextAnchor = "projects-page";
     }
 });
 
@@ -36,6 +61,7 @@ var waypointProjectsTop = new Waypoint({
     element: document.getElementById('projects-page'),
     handler: function () {
         toggleActive('projects-link');
+        nextAnchor = "contact-page";
     },
     offset: '50%'
 });
@@ -44,6 +70,7 @@ var waypointProjectsBottom = new Waypoint({
     element: document.getElementById('projects-page'),
     handler: function () {
         toggleActive('projects-link');
+        nextAnchor = "contact-page";
     }
 });
 
@@ -51,6 +78,7 @@ var waypointContactTop = new Waypoint({
     element: document.getElementById('contact-page'),
     handler: function () {
         toggleActive('contact-link');
+        nextAnchor = "main-page";
     },
     offset: '60%'
 });
@@ -59,5 +87,6 @@ var waypointContactBottom = new Waypoint({
     element: document.getElementById('contact-page'),
     handler: function () {
         toggleActive('contact-link');
+        nextAnchor = "main-page";
     }
 });
