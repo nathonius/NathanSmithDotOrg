a3e498ee339f710288a11ffad28973861fedfe1f
Nathan Smith <nathan@praisetopia.org>
2018-02-21 12:09:54 -0600

    added first blog post

--- public/blog/postlist.js
+++ public/blog/postlist.js
@@ -1,7 +1,12 @@
 const posts = [
+    // {
+    //     title: "template",
+    //     href: "template",
+    //     date: "2/15/2018"
+    // },
     {
-        title: "template",
-        href: "template",
-        date: "2/15/2018"
+        title: "frustration",
+        href: "frustration",
+        date: "2/21/2018"
     }
 ]
