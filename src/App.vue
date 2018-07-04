eb2112e61f0f027a1ac0c25e342724af5a67dfe5
Nathan Smith <nathan@praisetopia.org>
2018-07-04 01:59:07 -0500

    it is working a bit

--- src/App.vue
+++ src/App.vue
@@ -1,16 +1,22 @@
-<template src="./App.html"></template>
-
+<style lang="scss" src="./App.style.scss"></style>
+<template>
+    <div id="page-wrapper">
+        <NavBar></NavBar>
+        <div id="sections">
+            <div class="section" id="section-about">About</div>
+            <div class="section" id="section-projects">Projects</div>
+            <div class="section" id="section-contact">Contact</div>
+        </div>
+    </div>
+</template>
 <script lang="ts">
-import { Component, Vue } from 'vue-property-decorator';
-import NavBar from './components/NavBar.vue';
-import Project from './components/Project.vue';
+    import { Component, Vue } from 'vue-property-decorator';
+    import NavBar from './components/NavBar/NavBar.vue';
 
-@Component({
-  components: {
-  },
-})
-export default class App extends Vue {}
+    @Component({
+        components: {
+            NavBar
+        }
+    })
+    export default class App extends Vue {}
 </script>
-
-<style>
-</style>
--- src/components/NavBar/NavBar.vue
+++ src/components/NavBar/NavBar.vue
@@ -1,6 +1,20 @@
+<template>
+    <nav>
+        <ul>
+            <li>Home</li>
+            <li>About</li>
+            <li>Projects</li>
+            <li>Contact</li>
+        </ul>
+    </nav>
+</template>
 <script lang="ts">
-import Vue from 'vue'
-export default class NavBar extends Vue {
-    
-}
+    import { Vue, Component } from 'vue-property-decorator';
+
+    @Component
+    export default class NavBar extends Vue {
+    }
 </script>
+<style lang="scss">
+
+</style>
