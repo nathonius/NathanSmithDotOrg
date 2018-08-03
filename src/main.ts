a84e98f43fb081dd3ec50ddd3d5e3d254a104597
Nathan Smith <nathan@praisetopia.org>
2018-08-02 22:41:29 -0500

    added 404 page

--- src/main.ts
+++ src/main.ts
@@ -1,13 +1,14 @@
 import Vue from 'vue';
-import VueRouter from 'vue-router';
+import VueRouter, { RouteConfig } from 'vue-router';
 import App from './views/App.vue';
-import Test from './views/Test.vue';
+import FourOFour from './views/FourOFour.vue';
 
 Vue.config.productionTip = false;
 Vue.use(VueRouter);
 
-const routes = [
-    { path: '/', component: App }
+const routes: RouteConfig[] = [
+    { path: '/', component: App },
+    { path: '*', component: FourOFour }
 ];
 
 const router = new VueRouter({
---
+++ src/views/FourOFour.vue
@@ -0,0 +1,49 @@
+<template>
+    <div id="wrapper404">
+        <h2>Sorry, this page doesn't exist yet.</h2>
+        <h1>404</h1>
+        <h2>Maybe it should exist, or something is broken? Email me:</h2>
+        <a href="mailto:nathan@nathan-smith.org"><h2>nathan@nathan-smith.org</h2></a>
+        <br>
+        <h2>Or <a href="#" v-on:click="GoBack">go back.</a></h2>
+    </div>
+</template>
+<script lang="ts">
+import { Component, Vue } from 'vue-property-decorator';
+
+@Component
+export default class FourOFour extends Vue {
+    public GoBack(): void {
+        window.history.back();
+    }
+}
+</script>
+<style lang="scss">
+@import '../Global.scss';
+
+#wrapper404 {
+    height: 100vh;
+    width: 100vw;
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    justify-content: center;
+    margin-left: auto;
+    margin-right: auto;
+    text-align: center;
+    h1 {
+        font-family: "Quicksand", sans-serif;
+        font-size: 10em;
+    }
+    a {
+        &, &:visited, &:link {
+            color: $blue;
+        }
+        &:hover, &:active {
+            color: $black;
+        }
+    }
+}
+
+</style>
+
